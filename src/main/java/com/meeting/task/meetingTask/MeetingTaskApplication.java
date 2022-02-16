3d937e84cdc852ce1307e06e262daf906db9fb97
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2022-02-16 16:15:42 -0300

    first commit

---
+++ src/main/java/com/meeting/task/meetingTask/MeetingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.meeting.task.meetingTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingTaskApplication.class, args);
+	}
+
+}
